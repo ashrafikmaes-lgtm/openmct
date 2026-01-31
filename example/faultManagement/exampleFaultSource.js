1de25f4c5e32c5f1b4c593d1cfb68f3a6f3df22a
ashrafikmaes-lgtm <ashrafikmaes@gmail.com>
2026-01-31 07:23:31 +0300

    Update exampleFaultSource.js

--- example/faultManagement/exampleFaultSource.js
+++ example/faultManagement/exampleFaultSource.js
@@ -1,3 +1,8 @@
+
+
+
+
+
 /*****************************************************************************
  * Open MCT, Copyright (c) 2014-2024, United States Government
  * as represented by the Administrator of the National Aeronautics and Space
